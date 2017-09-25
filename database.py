0cc63925df32a71889d5acefed4c44113a303716
Lidia Mitroshina <lgliducik@gmail.com>
2017-09-25 17:17:16 +0400

    create table, add actions

---
+++ database.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+from flask.ext.sqlalchemy import SQLAlchemy
+
+DB_CONNECTOR = 'sqlite:///week.db'
+
+
+class ORM(object):
+
+    def __init__(self, app):
+        self.db = SQLAlchemy(app)
+
+        class Action(self.db.Model):
+            __tablename__ = 'action'
+
+            id = self.db.Column(self.db.Integer, primary_key=True)
+            name = self.db.Column(self.db.String)
+            freq = self.db.Column(self.db.Integer)
+
+            def __init__(self, name, freq):
+                self.name = name
+                self.freq = freq
+				
+
+            def __repr__(self):
+                return "<Action('%s', '%d')>" % (self.name, self.freq)
+
+
+        self.Action = Action
+        self.db.create_all()
+		
+        class ActionDate(self.db.Model):
+            __tablename__ = 'actiondate'
+
+            id = self.db.Column(self.db.Integer, primary_key=True)
+            name = self.db.Column(self.db.String)
+            date = self.db.Column(self.db.DateTime)
+
+            def __init__(self, name, date):
+                self.name = name
+                self.date = date
+				
+
+            def __repr__(self):
+                return "<ActionDate('%s', '%s')>" % (self.name, self.date)
+		
+		
+        self.ActionDate = ActionDate
+        self.db.create_all()
+        
+    def add_action(self, name, freq):
+        action = self.Action(name, freq)
+        self.db.session.add(action)
+        self.db.session.commit()
+
+    def delete_action(self, name):
+        action = self.Action.query.filter_by(name=name).first()
+        self.db.session.delete(action)
+        self.db.session.commit()
+
+    def all_actions(self):
+        return self.Action.query.all()
+		
+    def add_point(self, name, date):
+        actiondate = self.ActionDate(name, date)
+        self.db.session.add(actiondate)
+        self.db.session.commit()
+
--- server.py
+++ server.py
@@ -3,18 +3,50 @@ import os
 import logging
 
 from flask import Flask, request
-
+from flask import render_template, request, url_for, redirect, session
+from database import DB_CONNECTOR, ORM
 
 app = Flask(__name__)
+
 logger = logging.getLogger(__name__)
+logging.basicConfig(stream = sys.stdout, level=logging.DEBUG)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_CONNECTOR
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+logger.info('create db')
+db = ORM(app)
 
 
-@app.route("/", methods=["GET", "POST"])
-def root():
-    logger.info("request processing")
+logger = logging.getLogger(__name__)
+		
+		
+@app.route("/addaction", methods=["POST"])
+def addaction():
+    if request.method == "POST":
+        name = request.form.get("name")
+        freq = request.form.get("freq")
+        db.add_action(name, freq)
+        return redirect(url_for("showaction"))
+		
+		
+@app.route("/delaction", methods=["GET"])
+def delaction():
     if request.method == "GET":
         return "Hello"
-
+		
+		
+@app.route("/showaction", methods=["GET", "POST"])
+def showaction():
+    if request.method == "GET":
+        actions = db.all_actions()
+        print actions
+        return render_template('hello.html', actions=actions)
+    if request.method == "POST":
+        name = request.form.get("name")
+        freq = request.form.get("freq")
+        db.add_action(name, freq)
+        return redirect(url_for("showaction"))
+		
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
